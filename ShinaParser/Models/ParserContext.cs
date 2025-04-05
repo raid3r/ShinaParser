8bf1b4b154923ed1df787df68d39f9e33fb5d1fc
Vasyl Kovalov <raid3r81@gmail.com>
2025-04-05 14:44:16 +0300

    Добавлен метод OnModelCreating и настройки моделей
    В класс ParserContext добавлен метод OnModelCreating, который настраивает модель с использованием Fluent API.
    
    Для сущности Brand:
    - Установлено имя таблицы как product_brands.
    - Определен первичный ключ по полю Id.
    - Установлено имя столбца для поля Id как brand_id и добавлено использование автоинкремента.
    - Установлено имя столбца для поля Title как brand_name, добавлено ограничение на обязательность и максимальную длину в 100 символов.
    
    Для сущности Country:
    - Установлено имя таблицы как countries.
    - Определен первичный ключ по полю Id.
    - Установлено использование автоинкремента для поля Id.
    - Добавлено ограничение на обязательность и максимальную длину в 100 символов для поля Title.
    - Добавлен уникальный индекс на поле Title с именем индекса IX_countries_Title.
    
    Для сущности Product:
    - Добавлено отношение "один ко многим" с сущностью Country через внешний ключ CountryId, который может быть необязательным.
    - Установлено имя столбца для поля CountryId как country_model_id.

--- ShinaParser/Models/ParserContext.cs
+++ ShinaParser/Models/ParserContext.cs
@@ -17,4 +17,72 @@ public class ParserContext : DbContext
     {
         optionsBuilder.UseSqlServer("Data Source=SILVERSTONE\\SQLEXPRESS;Initial Catalog=ShinaParserP33;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Connect Timeout=60;Encrypt=True;Trust Server Certificate=True;Command Timeout=0");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Brand
+
+        modelBuilder.Entity<Brand>()
+            // Fluent API - set table name
+            .ToTable("product_brands")
+            // primary key
+            .HasKey(b => b.Id);
+
+        // set column name for Id   
+        modelBuilder.Entity<Brand>()
+            .Property(b => b.Id)
+            //.HasColumnType("BIGINT")
+            .HasColumnName("brand_id")
+            .UseIdentityColumn();
+
+        // set column name for Title
+        modelBuilder.Entity<Brand>()
+            .Property(b => b.Title)
+            .HasColumnName("brand_name")
+            .IsRequired()
+            .HasMaxLength(100);
+
+        // Country
+        modelBuilder.Entity<Country>()
+            // Fluent API - set table name
+            .ToTable("countries")
+            // primary key
+            .HasKey(c => c.Id);
+        // set column name for Id
+        modelBuilder.Entity<Country>()
+            .Property(c => c.Id)
+            //.HasColumnName("country_id")
+            .UseIdentityColumn();
+
+        modelBuilder.Entity<Country>()
+            .Property(c => c.Title)
+            //.HasColumnName("country_name")
+            .IsRequired()
+            .HasMaxLength(100);
+
+        modelBuilder.Entity<Country>()
+            .HasIndex(c => c.Title)
+            .IsUnique()
+            .HasDatabaseName("IX_countries_Title");
+
+
+
+        modelBuilder.Entity<Product>()
+            // - foreign key
+            .HasOne<Country>(p => p.Country)
+            .WithMany(c => c.Products)
+            .HasForeignKey(p => p.CountryId)
+            .IsRequired(false);
+
+        modelBuilder.Entity<Product>()
+            // - foreign key
+            .Property(p => p.CountryId)
+            .HasColumnName("country_model_id");
+
+
+
+
+    }
 }
