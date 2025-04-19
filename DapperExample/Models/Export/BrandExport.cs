98e65c7f2275f8a7e332a209119ab264962fa90a
Vasyl Kovalov <raid3r81@gmail.com>
2025-04-19 14:50:51 +0300

    Добавлены классы для экспорта данных брендов и стран
    В файле `BrandExport.cs` добавлен класс `BrandExport` с свойствами `Id` и `Title`, которые игнорируются при сериализации.
    
    В файле `CountryExport.cs` добавлен класс `CountryExport` с аналогичными свойствами.
    
    В файле `ProductExport.cs` добавлен класс `ProductExport`, содержащий множество свойств, включая `Id`, `Title`, `Article`, `Price`, `ImageUrl`, `Season`, `Size`, `Year`, `IsAvailable`, а также свойства `CountryId` и `BrandId`, которые также игнорируются при сериализации. Класс включает навигационные свойства `Country` и `Brand`, ссылающиеся на `CountryExport` и `BrandExport`.

---
+++ DapperExample/Models/Export/BrandExport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DapperExample.Models.Export;
+
+public class BrandExport
+{
+    [JsonIgnore]
+    [XmlIgnore]
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+}
---
+++ DapperExample/Models/Export/ProductExport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
+namespace DapperExample.Models.Export;
+
+public class ProductExport
+{
+    [JsonPropertyName("id")]
+    [XmlElement("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("title")]
+    [XmlElement("title")]
+    public string Title { get; set; } = null!;
+    [JsonPropertyName("num")]
+    [XmlElement("num")]
+    public string Article { get; set; } = null!;
+    public decimal? Price { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Season { get; set; }
+    public string? Size { get; set; }
+    public int? Year { get; set; }
+    public bool IsAvailable { get; set; }
+
+    [JsonIgnore]
+    [XmlIgnore]
+    public int? CountryId { get; set; }
+    [JsonIgnore]
+    [XmlIgnore]
+    public int BrandId { get; set; }
+
+    [JsonPropertyName("country")]
+    [XmlElement("country")]
+    public virtual CountryExport? Country { get; set; } = null!;
+    [JsonPropertyName("brand")]
+    [XmlElement("brand")]
+    public virtual BrandExport Brand { get; set; } = null!;
+}
